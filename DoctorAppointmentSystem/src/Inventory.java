15a44a5381d1faee2e667f42a7dc8939a9cd2ed3
marva00 <marvaasif00@gmail.com>
2023-11-17 16:29:05 +0500

    by marva(not final)

--- DoctorAppointmentSystem/src/Inventory.java
+++ DoctorAppointmentSystem/src/Inventory.java
@@ -5,24 +5,26 @@ public class Inventory {
     ArrayList<Patient> patientList = new ArrayList<>(50);
 
         public void add(Doctor doctor) {
+
             docList.add(doctor);
         }
 
         public void add(Patient patient) {
+
             patientList.add(patient);
         }
 
         public void remove(Doctor doctor){
-           docList.remove(doctor);
+
+            docList.remove(doctor);
         }
 
         public void remove(Patient patient){
-                patientList.remove(patient);
+
+            patientList.remove(patient);
             }
 
-            public Doctor searchDoctor(){
 
-            }
 
 
         }
